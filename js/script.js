bf28f7b561b76fef7144af14d7225e15f30bd380
Matheus <qmatheusnogueira@gmail.com>
2026-06-08 15:12:56 -0300

    adicionando funcionalidades javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector("#menu-btn");
+
+const nav = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click",()=>{
+
+nav.classList.toggle("active");
+
+});
